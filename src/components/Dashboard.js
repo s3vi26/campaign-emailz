3e493c5d058071fface3f2ae340a4491775986d8
Nicole Sevillano <34521909+s3vi26@users.noreply.github.com>
2021-03-23 19:31:40 -0400

    axios call getting preflight error

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -22,12 +22,22 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const zipcodes = (members) => {
+  const zips = [];
+
+  members.forEach(member =>{
+    if (zips.includes(member.zip_code)) {
+      console.log('already in array. skipping value');
+    } else {
+      zips.push(member.zip_code)
+    }
+  });
+  return zips;
+}
+
 const Dashboard = () => {
-  // You can use this file as the Campaign Dashboard
-  // Add the campaigns here
-  // Write to the console to show what is happening in the app
   const classes = useStyles();
-  const [emails, ] = useState([]);
+  const [emails, setEmails] = useState([]);
   const [message_id, setMessage_id] = useState('');
   const [members, ] = useState(membersData);
   const [messages, ] = useState(messageData);
@@ -35,21 +45,6 @@ const Dashboard = () => {
   const [beforeDate, setBeforeDate] = useState(new Date());
   const [zipcode, setZipcode] = useState('');
   const [month, setMonth] = useState('');
-  console.log(members);
-  console.log(messages);
-
-  const zipcodes = (members) => {
-    const zips = [];
-
-    members.forEach(member =>{
-      if (zips.includes(member.zip_code)) {
-        console.log('already in array. skipping value');
-      } else {
-        zips.push(member.zip_code)
-      }
-    });
-    return zips;
-  }
 
   const zips = zipcodes(members);
 
@@ -58,13 +53,24 @@ const Dashboard = () => {
     switch (attribute) {
       case 'birth_date':
         // get emails for members w/ birth_date in month value
+        const dobMatches = await members.filter(member => new Date(member.birth_date).getMonth() === month);
+        dobMatches.forEach(match => {
+          emails.push(match.email);
+        });
         break;
-      
       case 'join_date':
         // get emails for memebers w/ join_date before beforeDate value
+        const jdMatches = await members.filter(member => new Date(member.join_date) < beforeDate);
+        jdMatches.forEach(match => {
+          emails.push(match.email);
+        })
         break;
       case 'zip_code':
         // get emails for memebers w/ zip_code in zipcode value
+        const zipMatches = await members.filter(member => member.zip_code === zipcode);
+        zipMatches.forEach(match => {
+          emails.push(match.email);
+        })
         break;
         
       default:
@@ -74,34 +80,39 @@ const Dashboard = () => {
       const res = await axios({
         method: 'POST',
         url: 'https://qa8lmqoir2.execute-api.us-east-1.amazonaws.com/dev/members/send',
+        headers: {
+          'Content-Type': "application/json"
+        },
         data: {
           emails,
           message_id
         }
       });
       console.log(res);
+      setEmails([]);
+      setMessage_id('');
+      setAttribute('');
+      setBeforeDate(new Date());
+      setZipcode('');
+      setMonth('');
     } catch (e) {
       console.log(e);
     }
   }
 
   const messageIdChange = (e) => {
-    console.log(e.target.value);
     setMessage_id(e.target.value);
   }
 
   const attributeChange = (e) => {
-    console.log(e.target.value);
     setAttribute(e.target.value);
   }
 
   const monthChange = (e) => {
-    console.log(e.target.value);
     setMonth(e.target.value);
   }
 
   const zipChange = (e) => {
-    console.log(e.target.value);
     setZipcode(e.target.value);
   }
 
@@ -142,18 +153,18 @@ const Dashboard = () => {
             value={month}
             onChange={monthChange}
           >
-            <MenuItem value="01">Jan</MenuItem>
-            <MenuItem value="02">Feb</MenuItem>
-            <MenuItem value="03">Mar</MenuItem>
-            <MenuItem value="04">Apr</MenuItem>
-            <MenuItem value="05">May</MenuItem>
-            <MenuItem value="06">Jun</MenuItem>
-            <MenuItem value="07">Jul</MenuItem>
-            <MenuItem value="08">Aug</MenuItem>
-            <MenuItem value="09">Sep</MenuItem>
-            <MenuItem value="10">Oct</MenuItem>
-            <MenuItem value="11">Nov</MenuItem>
-            <MenuItem value="12">Dec</MenuItem>
+            <MenuItem value={0}>Jan</MenuItem>
+            <MenuItem value={1}>Feb</MenuItem>
+            <MenuItem value={2}>Mar</MenuItem>
+            <MenuItem value={3}>Apr</MenuItem>
+            <MenuItem value={4}>May</MenuItem>
+            <MenuItem value={5}>Jun</MenuItem>
+            <MenuItem value={6}>Jul</MenuItem>
+            <MenuItem value={7}>Aug</MenuItem>
+            <MenuItem value={8}>Sep</MenuItem>
+            <MenuItem value={9}>Oct</MenuItem>
+            <MenuItem value={10}>Nov</MenuItem>
+            <MenuItem value={11}>Dec</MenuItem>
           </Select>
         </FormControl>
       }
@@ -186,7 +197,7 @@ const Dashboard = () => {
           >
             {zips.map(zip => {
               return(
-                <MenuItem value={zip}>{zip}</MenuItem>
+                <MenuItem key={zip.toString()} value={zip}>{zip}</MenuItem>
               )
             })
             }
